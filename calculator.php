00b447d9c100274f249e8a778b25778746ab2ed8
Maurice Arnaud <arnaud.maurice.pro@gmail.com>
2017-12-21 11:34:35 +0100

    add submodule php-multi-lib

--- calculator.php
+++ calculator.php
@@ -3,8 +3,10 @@ echo "1 Add \n2 Multi \n3 Quit";
 
 $choice = trim(fgets(STDIN));
 
+$addClass = new AddCommand();
 switch ($choice) {
     case 1:
+        $addClass->add();
         echo 'bobby';
         break;
     case 2:
