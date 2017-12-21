2607185f6e41b02823c450ae73563819bc4aee2f
Maurice Arnaud <arnaud.maurice.pro@gmail.com>
2017-12-21 11:43:43 +0100

    Ajout fonctions ajout et multiplication

--- calculator.php
+++ calculator.php
@@ -1,20 +1,43 @@
 <?php
-echo "1 Add \n2 Multi \n3 Quit";
+require_once "./php-add-lib/AddCommand.php";
+require_once "./php-multi-lib/MultiCommand.php";
+echo "1 Add \n2 Multi \n3 Quit\n";
 
 $choice = trim(fgets(STDIN));
 
-$addClass = new AddCommand();
 switch ($choice) {
     case 1:
-        $addClass->add();
-        echo 'bobby';
+        echo "Le résultat est ".addChoice();
         break;
     case 2:
-        echo 'bobby';
+        echo "Le résultat est ".multiChoice();
         break;
     case 3:
         echo 'Aurevoir';
         break;
     default:
         echo "default";
+        break;
+}
+
+/**
+ * @return int
+ */
+function addChoice()
+{
+    $addClass = new AddCommand();
+    echo "\nVeuillez saisir un nombre\n";
+    $number = trim(fgets(STDIN));
+    return $addClass->add($number, $number);
+}
+
+/**
+ * @return int
+ */
+function multiChoice()
+{
+    $multiClass = new MultiCommand();
+    echo "\nVeuillez saisir un nombre\n";
+    $number = trim(fgets(STDIN));
+    return $multiClass->multi($number, $number);
 }
